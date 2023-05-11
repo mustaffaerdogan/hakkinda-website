baa4b4cdbef6926d5eefcdeaddac1a55d4337339
mustaffaerdogan <49840036+mustaffaerdogan@users.noreply.github.com>
2023-05-11 17:36:03 +0300

    clear btn + ilgi api added

---
+++ js/main.js
@@ -0,0 +1,45 @@
+
+function temizleForm() {
+    // Form alanlarına erişip ve formu temizleriz.
+    document.getElementById("form-ad").value = "";
+    document.getElementById("form-mail").value = "";
+    document.getElementById("form-mesaj").value = "";
+}
+
+
+fetch('https://app.sheetlabs.com/RAPI/Top100movieslist')
+    .then(response => response.json())
+    .then(data => {
+        // REST API veri çekme işlemleri.
+        console.log(data);
+
+        document.getElementById("movie-desc1").textContent = data[0].description;
+        document.getElementById("movie-desc2").textContent = data[1].description;
+        document.getElementById("movie-desc3").textContent = data[2].description;
+        document.getElementById("movie-desc4").textContent = data[3].description;
+        document.getElementById("movie-desc5").textContent = data[4].description;
+        document.getElementById("movie-desc6").textContent = data[5].description;
+
+
+        document.getElementById("movie-title1").textContent = data[0].title;
+        document.getElementById("movie-title2").textContent = data[1].title;
+        document.getElementById("movie-title3").textContent = data[2].title;
+        document.getElementById("movie-title4").textContent = data[3].title;
+        document.getElementById("movie-title5").textContent = data[4].title;
+        document.getElementById("movie-title6").textContent = data[5].title;
+
+
+        document.getElementById("movie-img1").setAttribute("src", data[0].image)
+        document.getElementById("movie-img2").setAttribute("src", data[1].image)
+        document.getElementById("movie-img3").setAttribute("src", data[2].image)
+        document.getElementById("movie-img4").setAttribute("src", data[3].image)
+        document.getElementById("movie-img5").setAttribute("src", data[4].image)
+        document.getElementById("movie-img6").setAttribute("src", data[5].image)
+
+
+    })
+    .catch(error => {
+        // Hata durumunda burası çalışır
+        console.error('Hata veriler çekilemiyor. :', error);
+    });
+
